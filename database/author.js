ef62fee41d10a785062fcce7de85068fa793b5bf
Karthikakrzy <crazykarthiomk@gmail.com>
2021-07-01 00:06:52 -0700

    add mongoose models

---
+++ database/author.js
@@ -0,0 +1,15 @@
+cost mongoose = require("mongoose");
+
+
+
+// Author Schema
+const AuthorSchema = mongoose.Schema({
+    id: Number,
+    name: String,
+    books: [String],
+  });
+  
+  // Author Model
+  const AuthorModel = mongoose.model(AuthorSchema);
+  
+  module.exports = AuthorModel;
