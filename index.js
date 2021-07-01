3d264634ae8bed251bdf516c0e73efa88d10d166
Karthikakrzy <crazykarthiomk@gmail.com>
2021-07-01 02:14:32 -0700

    add mongodb connection

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ parameters         NONE
 method             GET
 */
 
-shapeAI.get("/", (req,res)=>{
-    //hello change
-return res.json({books:database.books});
+shapeAI.get("/", async(req,res)=>{
+    const getAllBooks = await BookModel.find();
+return res.json(getAllBooks);
 }
 );
 
@@ -51,12 +51,13 @@ parameters         isbn
 method             GET
 */
 
-shapeAI.get("/is/:isbn",(req, res)=>{
-    const getSpecificBook =database.books.filter(
-        (book)=> book.ISBN===req.params.isbn
-        );
+shapeAI.get("/is/:isbn",async (req, res)=>{
 
-        if(getSpecificBook.length===0){
+const getSpecificBook = await BookModel.findOne({ISBN:req.params.isbn});
+
+//null->false
+
+ if(!getSpecificBook){
         return res.json({error: `No book found for the ISBN of ${req.params.isbn}`,
     });
         }
@@ -71,12 +72,11 @@ access             PUBLIC
 parameters         category
 method             GET
 */
-shapeAI.get("/c/:category",(req, res)=>{
-    const getSpecificBooks =database.books.filter(
-        (book)=> book.category.includes(req.params.category)
-        );
+shapeAI.get("/c/:category",async (req, res)=>{
+const getSpecificBooks= await BookModel.findOne({category:req.params.category})
 
-        if(getSpecificBooks.length===0){
+
+        if(!getSpecificBooks){
         return res.json({error: `No book found for the category of ${req.params.category}`,
     });
         }
@@ -91,8 +91,9 @@ parameters         category
 method             GET
 */
 
-shapeAI.get("/author", (req, res)=>{
-    return res.json({authors : database.authors});
+shapeAI.get("/author", async (req, res)=>{
+    const getAllAuthors = await AuthorModel.find();
+    return res.json({authors : getAllAuthors});
 });
 
 /*
@@ -139,12 +140,12 @@ parameters         NONE
 method             POST
 */
 
-shapeAI.post("/book/new",(req,res)=>{
+shapeAI.post("/book/new",async (req,res)=>{
 const {newBook}= req.body;
 
-database.books.push(newBook);
+BookModel.create(newBook);
 
-return res.json({books: database.books, message:"book was added!"});
+return res.json({message:"book was added!"});
 });
 
 /*
@@ -157,9 +158,9 @@ method             POST
 shapeAI.post("/author/new",(req,res)=>{
     const {newAuthor}= req.body;
     
-    database.authors.push(newAuthor);
+    AuthorModel.create(newAuthor);
     
-    return res.json({authors: database.authors, message:"author was added!"});
+    return res.json({ message:"author was added!"});
     });
 
 /*
